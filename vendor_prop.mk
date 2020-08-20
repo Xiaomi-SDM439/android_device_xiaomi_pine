a0086069e171559e7876ce50ffa3c28323a5ba4d
Louis Popi <theh2o64@gmail.com>
2020-08-20 17:40:27 +0200

    pine: Set vendor props via PRODUCT_PROPERTY_OVERRIDES
    Change-Id: I3ac626a19c2694961a07ae60f9e2597e3594fac6

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,7 +1,14 @@
-PRODUCT_PROPERTY_OVERRIDES +=  \
+PRODUCT_PROPERTY_OVERRIDES += \
+	audio_para_version=C3E-Audiopara-V30-20190929 \
+	camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
 	dalvik.vm.dex2oat-threads=6 \
+	debug.egl.hw=1 \
+	debug.sf.hw=1 \
+	persist.dpm.feature=1 \
 	persist.vendor.sensor.hw.binder.size=8 \
 	rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
+	ro.audio.soundfx.dirac=true \
+	ro.config.max_starting_bg=8 \
 	ro.vendor.audio.sos=true \
 	vendor.mediacodec.binder.size=4 \
 	vendor.rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
